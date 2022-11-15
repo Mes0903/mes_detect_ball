7a9f7ee0c64029e0db5a0ccd404eb37ba2de5a92
Mes <sandstorm0903@gmail.com>
2022-11-15 13:31:40 +0800

    11/15

--- mes_detect_ball_original_code/include/header/make_feature.h
+++ mes_detect_ball_original_code/include/header/make_feature.h
@@ -71,6 +71,7 @@ Eigen::VectorXd make_feature(const Eigen::MatrixXd &Seg)
   return feature;
 }
 
+/*
 std::pair<Eigen::MatrixXd, std::vector<Eigen::MatrixXd>> transform_to_feature(const Eigen::MatrixXd &xy_data)
 {
   std::vector<Eigen::MatrixXd> segment_data;
@@ -106,6 +107,35 @@ std::pair<Eigen::MatrixXd, std::vector<Eigen::MatrixXd>> transform_to_feature(co
   }
 
   return {feature_data, segment_data};
+}
+*/
+
+std::pair<Eigen::MatrixXd, std::vector<Eigen::MatrixXd>> transform_to_feature(const Eigen::MatrixXd &xy_data) // xy_data is 720*2
+{
+
+  Eigen::MatrixXd feature_data = Eigen::MatrixXd::Zero(1, 5);
+  bool empty_flag = true;
+
+  [[maybe_unused]] int SECTION = xy_data.rows() / 720;
+
+  std::vector<Eigen::MatrixXd> section_seg_vec = do_section_segment(xy_data); // 那一秒切出來的所有 segment
+
+  for (const auto &Seg : section_seg_vec)
+  {
+    Eigen::VectorXd single_feature = make_feature(Seg);
+
+    if (empty_flag)
+    {
+      feature_data += single_feature.transpose();
+      empty_flag = false;
+    }
+    else
+    {
+      feature_data = AppendRow(feature_data, single_feature);
+    }
+  }
+
+  return {feature_data, section_seg_vec};
 }
 
 #endif
